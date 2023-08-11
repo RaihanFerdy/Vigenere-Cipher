675e3ba128ba22290ddfd80ba0648af6a22b2d75
AtsuFerdy <raihanferdy@gmail.com>
2023-08-11 10:58:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vigenere Cipher
+Vigenere Cipher is a method of encryption or encoding used to secure messages by combining the Caesar Cipher with a series of different Caesar ciphers based on a key known as the "Vigenere key."
+
+![Vigenere Cipher](https://i.imgur.com/rtFTBqV.png)
---
+++ main.py
@@ -0,0 +1,84 @@
+import string, os
+
+def encrypt(message, key):
+    result = ""
+    for i in range(len(message)):
+        char = message[i]
+        if char.isupper():
+            key = key.upper()
+            if len(key) < len(message):
+                key += key[i % len(key)]
+            elif len(key) > len(message):
+                key = key[i:len(message)]
+            result_index = (string.ascii_uppercase.index(char) + string.ascii_uppercase.index(key[i])) % 26
+            result += string.ascii_uppercase[result_index]
+        elif char.lower():
+            key = key.lower()
+            if len(key) < len(message):
+                key += key[i % len(key)]
+            elif len(key) > len(message):
+                key = key[i:len(message)]
+            result_index = (string.ascii_lowercase.index(char) + string.ascii_lowercase.index(key[i])) % 26
+            result += string.ascii_lowercase[result_index]
+        else:
+            result += char
+    
+    save_file =  input("Save file to txt? [y/n]: ")
+    if save_file == "y":
+        os.system("cls")
+        with open(f"encrypt.txt", 'w') as f:
+            f.write(f"Message: {message}\n")
+            f.write(f"Key: {key}\n")
+            f.write(f"Result: {result}")
+    
+    elif save_file == "n":
+        os.system("cls")
+        print(f"Message: {message}")
+        print(f"Key: {key}")
+        print(f"Result: {result}\n")
+
+
+def decrypt(message, key):
+    result = ""
+    for i in range(len(message)):
+        char = message[i]
+        if char.isupper():
+            key = key.upper()
+            if len(key) < len(message):
+                key += key[i % len(key)]
+            elif len(key) > len(message):
+                key = key[i:len(message)]
+            result_index = (string.ascii_uppercase.index(char) - string.ascii_uppercase.index(key[i])) % 26
+            result += string.ascii_uppercase[result_index]
+        elif char.lower():
+            key = key.lower()
+            if len(key) < len(message):
+                key += key[i % len(key)]
+            elif len(key) > len(message):
+                key = key[i:len(message)]
+            result_index = (string.ascii_lowercase.index(char) - string.ascii_lowercase.index(key[i])) % 26
+            result += string.ascii_lowercase[result_index]
+        else:
+            result += char
+            
+    save_file =  input("Save file to txt? [y/n]: ")
+    if save_file == "y":
+        os.system("cls")
+        with open(f"decrypt.txt", 'w') as f:
+            f.write(f"Message: {message}\n")
+            f.write(f"Key: {key}\n")
+            f.write(f"Result: {result}")
+    
+    elif save_file == "n":
+        os.system("cls")
+        print(f"Message: {message}")
+        print(f"Key: {key}")
+        print(f"Result: {result}\n")
+        
+message = input("Input message: ")
+key = input("Key: ")
+method = input("Select method: [e/d]: ").lower()
+if method == "e":
+    encrypt(message, key)
+elif method == "d":
+    decrypt(message, key)
